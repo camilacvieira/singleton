f101834c469cba05d4b46ea12d6d5802766cbdc7
Camila <ccvieira@ice.ufjf.br>
2021-11-25 11:39:09 -0300

    adicionando equipe

--- src/com/company/Lider.java
+++ src/com/company/Lider.java
@@ -7,18 +7,28 @@ package com.company;
 
 public class Lider {
     private String nome;
+    private String equipe;
 
     private Lider(){}
     private static Lider lider = new Lider();
-    public static synchronized Lider obterInstancia(){
+    public static Lider getInstance(){
         return lider;
     }
 
-    public String obterNome(){
-        return lider.nome;
+    public String getNome(){
+        return nome;
     }
 
-    public void setarNome(String nome){
+    public void setNome(String nome){
         this.nome = nome;
     }
+
+    public String getEquipe(){
+        return equipe;
+    }
+
+    public void setEquipe(String equipe){
+        this.equipe = equipe;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,15 @@ public class Main {
         Lider lider1, lider2, lider3;
         //lider1 = new Lider(); Não permite instanciar, contrutor privado
 
-        lider2 = Lider.obterInstancia();
-        lider2.setarNome("Camila");
-        lider3 = Lider.obterInstancia();
+        lider2 = Lider.getInstance();
+        lider2.setNome("Camila021");
+        lider2.setEquipe("equipe azul");
+        lider3 = Lider.getInstance();
 
         if(lider2 == lider3){
             System.out.println("Mesmo lider");
-            System.out.println("Nome do lider 2 = " + lider2.obterNome());
-            System.out.println("Nome do lider 3 = " + lider3.obterNome());
+            System.out.println("Nome do lider 2 = " + lider2.getNome());
+            System.out.println("Nome do lider 3 = " + lider3.getNome());
         }
     }
 }
